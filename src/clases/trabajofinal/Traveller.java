98b3f79cc303d614821669d105163d024d8ab84e
Vicenç Álvarez <vicens17@gmail.com>
2019-10-27 20:16:29 +0100

    Consigo que me reconozca el Gson añadiendo la libreria a la estructura del proyecto

--- src/clases/trabajofinal/Traveller.java
+++ src/clases/trabajofinal/Traveller.java
@@ -1,27 +1,34 @@
 package clases.trabajofinal;
 
-import java.util.Date;
+import java.io.*;
 
-public class Traveller {
+import java.util.Date;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class Traveller implements Serializable {
 
     private String name;
     private String lastName;
     private Date birthday;
 
+    public Traveller(String name, String lastName, Date birthday) {
+        this.name = name;
+        this.lastName = lastName;
+        this.birthday = birthday;
+    }
 
-
-
-    // Aqui suscribimos el metodo de equals() de la clase generica `Object` que es de java de forma generica
-    @Override
-    public boolean equals(Object obj) {
-        // Si el `Object` que le pasamos es de tipo `Traveller` pasará el if()
-        if (obj instanceof Traveller){
-            // Creamos una variable de tipo `Traveller` pasando el objeto a este tipo de objeto
-            Traveller objTraveller = (Traveller)obj;
-            // Creamos el boolean con la condicion de que los nombres de los dos sean iguales, y no si son la misma instancia
-            return this.name == objTraveller.name && this.lastName == objTraveller.lastName;
+    private void saveObject(Traveller traveller) {
+        try {
+            OutputStream fileOut = new FileOutputStream("/tmp/persona.json");
+            Writer writer = new OutputStreamWriter(fileOut);
+            Gson gson = new GsonBuilder().create();
+            gson.toJson(traveller, writer);
+            writer.close();
+            fileOut.close();
+        } catch (IOException ioEx) {
+            ioEx.printStackTrace();
         }
-        return false;
     }
 
 }
