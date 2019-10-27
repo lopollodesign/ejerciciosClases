e44fa30b39de9b070e86bff84e43170c87276a15
Vicenç Álvarez <vicens17@gmail.com>
2019-10-27 13:36:35 +0100

    Añado la lista de destinos a piñon en el Main. Imagino que sera temporal, no huele mal

--- src/clases/trabajofinal/Destination.java
+++ src/clases/trabajofinal/Destination.java
@@ -1,25 +1,14 @@
 package clases.trabajofinal;
 
 import java.awt.Point;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 
 public class Destination {
     private String name;
     private Point point;
 
-    private String[] NAMES = {"Berlín", "Munich", "París", "Barcelona", "Lyon", "Madrid", "Gijón", "Palma de Mallorca", "Tenerife", "Bogotá", "Lima", "Hong Kong"};
-    private Point[] POINTS = { new Point(3 ,4 ) , new Point(6 ,9 ), new Point(10 ,2 ), new Point(20 ,10 ), new Point(50 ,30 ) };
-
-    public Destination(){}
-
-    public void setPoint(Point point) {
-        this.point = point;
-    }
-
-    public void setName(String name) {
+    public Destination(String name, Point point) {
         this.name = name;
+        this.point = point;
     }
 
     public float getDistance( Destination arrival ){
@@ -30,20 +19,6 @@ public class Destination {
         double distance = Math.sqrt((Math.pow( (arrivalX - departureX) , 2)) + (Math.pow( (arrivalY - departureY) , 2)));
 
         return 5f * (float) distance;
-    }
-
-    List<Destination> createDestinations() {
-        List<Destination> newDestinations = new ArrayList<>();
-        Random random = new Random();
-        for (String name : this.NAMES) {
-            Destination newDestination = new Destination();
-            newDestination.setName(name);
-            int randomPointsIndex = random.nextInt(this.POINTS.length);
-            newDestination.setPoint(this.POINTS[randomPointsIndex]);
-            newDestinations.add(newDestination);
-        }
-        return newDestinations;
-
     }
 
     @Override
--- src/clases/trabajofinal/Main.java
+++ src/clases/trabajofinal/Main.java
@@ -1,6 +1,7 @@
 package clases.trabajofinal;
 
-import java.util.Date;
+import java.awt.*;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
@@ -8,10 +9,37 @@ public class Main {
     public static void main(String[] args) {
 
         // CREACION DE DESTINOS RANDOM
-        List<Destination> destinations = new Destination().createDestinations();
+        List<Destination> destinations = createDestinations();
 
         //OUTPUT
         listToString(destinations);
+    }
+
+    private static List<Destination> createDestinations() {
+
+        return Arrays.asList(
+                new Destination("Berlin", new Point(3, 33)),
+                new Destination("Munich", new Point(6, 30)),
+                new Destination("Paris", new Point(9, 27)),
+                new Destination("Palma", new Point(12, 24)),
+                new Destination("London", new Point(15, 21)),
+                new Destination("Lyon", new Point(18, 18)),
+                new Destination("Gijon", new Point(21, 15)),
+                new Destination("Bogota", new Point(24, 12)),
+                new Destination("Lima", new Point(27, 9)),
+                new Destination("Hong Kong", new Point(30, 6)),
+                new Destination("Madrid", new Point(33, 3)),
+                new Destination("Manchester", new Point(36, 6)),
+                new Destination("Luxemburgo", new Point(39, 9)),
+                new Destination("Rabat", new Point(42, 12)),
+                new Destination("Fez", new Point(45, 15)),
+                new Destination("Dublín", new Point(48, 18)),
+                new Destination("Tolousse", new Point(51, 21)),
+                new Destination("Granada", new Point(54, 24)),
+                new Destination("Santander", new Point(57, 27)),
+                new Destination("Mahón", new Point(60, 30))
+        );
+
     }
 
     private static void listToString(List objectsList) {
