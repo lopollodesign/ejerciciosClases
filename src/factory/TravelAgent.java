a110853ab74e8fe72f5bb058e49ac481adbe63f6
Vicenç Álvarez <vicens17@gmail.com>
2019-11-02 13:03:45 +0100

    Inicio los datos en el TravelAgent mediante un metodo aposta que comprueba que estan los datos

--- src/factory/TravelAgent.java
+++ src/factory/TravelAgent.java
@@ -1,4 +1,84 @@
 package factory;
 
+import models.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class TravelAgent {
+    private String SERIALIZABLE_PATH;
+    private String SERIALIZABLE_FORMAT;
+
+    private Destination[] destinations;
+    private Activity[] activities;
+    private Hotel[] hotels;
+    private Traveller[] travellers;
+    private TransportType[] transportTypes;
+
+    public TravelAgent(String path, String format) {
+        this.SERIALIZABLE_PATH = path;
+        this.SERIALIZABLE_FORMAT = format;
+    }
+
+    public void initData() {
+        try {
+            this.destinations = Serial.loadArrayDestinationModel(SERIALIZABLE_PATH,SERIALIZABLE_FORMAT);
+        } catch (IOException ioEx){
+            System.out.println("No hay datos de destinos");
+        }
+        try {
+            this.activities = Serial.loadArrayActivityModel(SERIALIZABLE_PATH,SERIALIZABLE_FORMAT);
+        } catch (IOException ioEx){
+            System.out.println("No hay datos de Actividades");
+        }
+        try {
+            this.hotels = Serial.loadArrayHotelModel(SERIALIZABLE_PATH,SERIALIZABLE_FORMAT);
+        } catch (IOException ioEx){
+            System.out.println("No hay datos de Hoteles");
+        }
+        try {
+            this.travellers = Serial.loadArrayTravellerModel(SERIALIZABLE_PATH,SERIALIZABLE_FORMAT);
+        } catch (IOException ioEx){
+            System.out.println("No hay datos de Viajeros");
+        }
+        try {
+            this.transportTypes = Serial.loadArrayTransportTypeModel(SERIALIZABLE_PATH,SERIALIZABLE_FORMAT);
+        } catch (IOException ioEx){
+            System.out.println("No hay datos de Tipos de Transporte");
+        }
+    }
+
+    public void saveData() {
+        Serial.saveArrayModel(Arrays.asList(this.destinations), SERIALIZABLE_PATH, "destinations", SERIALIZABLE_FORMAT);
+        Serial.saveArrayModel(Arrays.asList(this.hotels), SERIALIZABLE_PATH, "hotels", SERIALIZABLE_FORMAT);
+        Serial.saveArrayModel(Arrays.asList(this.activities), SERIALIZABLE_PATH, "activities", SERIALIZABLE_FORMAT);
+        Serial.saveArrayModel(Arrays.asList(this.travellers), SERIALIZABLE_PATH, "travellers", SERIALIZABLE_FORMAT);
+        Serial.saveArrayModel(Arrays.asList(this.transportTypes), SERIALIZABLE_PATH, "transportTypes", SERIALIZABLE_FORMAT);
+    }
+
+    public Destination[] getDestinations() {
+        return destinations;
+    }
+
+    public Travel createRandomTravel() {
+        // createTravel
+        return new Travel();
+    }
+
+    public Travel[] addTravel(Travel travel, Travel[] travels){
+        // Travel newTravel = createRandomTravel()
+        // travels.add(newTravel)
+        return travels;
+    }
+
+    public void deleteTravel(Travel travel){
+        // delete travel
+    }
+
+    public List getTravels(){
+        return new ArrayList<Travel>();
+    }
+
 }
--- src/models/Main.java
+++ src/models/Main.java
@@ -1,6 +1,6 @@
 package models;
 
-import factory.Serial;
+import factory.TravelAgent;
 
 import java.io.IOException;
 import java.util.*;
@@ -8,31 +8,17 @@ import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args) throws IOException {
         String SERIALIZABLE_PATH = "c:/json/";
         String SERIALIZABLE_FORMAT = "txt";
 
-//        Serial.saveArrayModel(destinations, SERIALIZABLE_PATH, "destinations", SERIALIZABLE_FORMAT);
-//        Serial.saveArrayModel(hotels, SERIALIZABLE_PATH, "hotels", SERIALIZABLE_FORMAT);
-//        Serial.saveArrayModel(activities, SERIALIZABLE_PATH, "activities", SERIALIZABLE_FORMAT);
-//        Serial.saveArrayModel(travellers, SERIALIZABLE_PATH, "travellers", SERIALIZABLE_FORMAT);
-//        Serial.saveArrayModel(transportTypes, SERIALIZABLE_PATH, "transportTypes", SERIALIZABLE_FORMAT);
+        TravelAgent travelAgent = new TravelAgent(SERIALIZABLE_PATH, SERIALIZABLE_FORMAT);
+        travelAgent.initData();
 
-        // DESERIALIZACION
-        Destination[] destinations = Serial.loadArrayDestinationModel(SERIALIZABLE_PATH,SERIALIZABLE_FORMAT);
-        Activity[] activities = Serial.loadArrayActivityModel(SERIALIZABLE_PATH,SERIALIZABLE_FORMAT);
-        Hotel[] hotels = Serial.loadArrayHotelModel(SERIALIZABLE_PATH,SERIALIZABLE_FORMAT);
-        Traveller[] travellers = Serial.loadArrayTravellerModel(SERIALIZABLE_PATH,SERIALIZABLE_FORMAT);
-        TransportType[] transportTypes = Serial.loadArrayTransportTypeModel(SERIALIZABLE_PATH,SERIALIZABLE_FORMAT);
-
-        // OUTPUT
+        Destination[] destinations = travelAgent.getDestinations();
         listToString(Arrays.asList(destinations), "Destinations");
-        listToString(Arrays.asList(hotels), "Hotels");
-        listToString(Arrays.asList(activities), "Activities");
-        listToString(Arrays.asList(travellers), "Travellers");
-        listToString(Arrays.asList(transportTypes), "Transport Types");
-    }
 
+    }
 
     private static void listToString(List objectsList, String listName) {
         String information;
