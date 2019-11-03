9966884fb02a7236725d56828be6a17bf95c5913
Vicenç Álvarez <vicens17@gmail.com>
2019-11-03 12:20:19 +0100

    Hago que el equals de los servicios deje la comparacion del destino con su propio equals, de forma que si cambiaramos este ultimo no nos afactaria al servicio.

--- src/models/Service.java
+++ src/models/Service.java
@@ -18,7 +18,9 @@ public abstract class Service {
     public boolean equals(Object obj) {
         if (obj instanceof Service){
             Service objService = (Service) obj;
-            return (this.name + this.destination).equals(objService.name + objService.destination);
+            // De dejo el equals del mismo Destination
+            // Si mas adelante se quisiera comparar de una forma mas concreta el Destino no me afectaria aqui.
+            return (this.name).equals(objService.name) && this.destination.equals(objService.destination);
         }
         return false;
     }
